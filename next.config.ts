c33bf088af5ba600ba9cf0612fd54571cdd451b5
Ali Hassan <alih9two@gmail.com>
2026-01-29 00:28:11 +0500

    fix bucket policy

--- next.config.ts
+++ next.config.ts
@@ -16,5 +16,15 @@ const nextConfig: NextConfig = {
       },
     ];
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'bxjzeegbxjhtbbahnigs.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 export default nextConfig;
